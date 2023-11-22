66e56a8b15902e30351811f958f2e76a44423391
ort <orthodoxplus@ya.ru>
2023-11-23 00:48:37 +0300

    fix feign for telegram

--- src/main/java/main/BotsService.java
+++ src/main/java/main/BotsService.java
@@ -1,13 +1,19 @@
 package main;
 
 import feign.Feign;
+import feign.Response;
 import feign.Target;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 
 import static java.net.URI.create;
 
@@ -17,8 +23,20 @@ public class BotsService {
 	private long adminId;
 	private final TelegramServiceHttp telegramServiceHttp = Feign.builder()
 			.encoder(new JacksonEncoder())
-			.decoder(new JacksonDecoder())
+			.decoder(this::decodeTelegramTypes)
 			.target(Target.EmptyTarget.create(TelegramServiceHttp.class));
+	private final Type[] telegramResponseTypes = {SendMessage.class, DeleteMessage.class};
+
+	private Object decodeTelegramTypes(Response response, Type type) {
+		for (Type telegramType : telegramResponseTypes) {
+			try {
+				return new JacksonDecoder().decode(response, telegramType);
+			} catch (IOException ignored) {
+			}
+		}
+
+		throw new RuntimeException("Unknown telegram type!");
+	}
 
 	public BotApiMethod<?> request(Bot bot, Update update) {
 		if (isAdmin(update)) {
--- src/main/java/main/TelegramServiceHttp.java
+++ src/main/java/main/TelegramServiceHttp.java
@@ -1,5 +1,6 @@
 package main;
 
+import feign.Headers;
 import feign.RequestLine;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -8,5 +9,6 @@ import java.net.URI;
 
 public interface TelegramServiceHttp {
 	@RequestLine("POST")
+	@Headers("content-type: application/json")
 	BotApiMethod<?> send(URI uri, Update update);
 }
